a416c7260a22dc345c4b34d2ed508c3e156b59ec
Anderson <you@example.com>
2023-11-23 13:28:13 -0500

    likes

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import View
-from .models import Post
+from django.views.generic import DetailView
+from .models import Like, Post
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
@@ -9,3 +10,21 @@ class HomeView(View):
             'posts':posts
         }   
         return render(request,'index.html',context)
+    
+
+    
+
+
+class DetailPost(DetailView):
+    model = Post
+    template_name = 'detail.html'
+    
+
+def LikeView(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    like_query = Like.objects.filter(user=request.user, post=post)
+    if like_query.exists():
+        like_query[0].delete()
+        return redirect('/')
+    Like.objects.create(user=request.user, post=post)
+    return redirect('/')
