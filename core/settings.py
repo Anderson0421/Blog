07636e79dedc2549393ef8169f757087f1faf286
Anderson <you@example.com>
2023-11-22 19:48:14 -0500

    goaws

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ if not DEBUG:
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-    AWS_ACCESS_KEY_KEY = env('AWS_ACCESS_KEY_KEY')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
 
