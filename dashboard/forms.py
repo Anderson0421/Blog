f93d144e0d1b200f5ba6ff4e1c85cacd8d7df077
Ander Dev <jelow0413@gmail.com>
2023-11-28 10:12:16 -0500

    estilos

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
 from core.models import Post
+from django.forms import ModelForm, Select, TextInput,Textarea
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ('title','author','content','categoria','slug')
-        
+        fields = ('title', 'author', 'content', 'categoria', 'slug')
+        widgets = {
+            'title': TextInput(attrs={'class': 'p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'author': TextInput(attrs={'class': 'p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'content': Textarea(attrs={'class': 'p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'categoria': Select(attrs={'class': 'p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+            'slug': TextInput(attrs={'class': 'p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300'}),
+        }
