8ce4e8c3e163508b18b9eef065c363fbb341e453
Ander Dev <jelow0413@gmail.com>
2023-11-28 13:29:39 -0500

    postgre

--- core/settings.py
+++ core/settings.py
@@ -97,13 +97,14 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
 LOGIN_REDIRECT_URL = '/'
 
 
