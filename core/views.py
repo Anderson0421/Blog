fe0dcd3c312095f565a830a6b443e4854148b912
Ander Dev <jelow0413@gmail.com>
2023-11-29 18:32:49 -0500

    deployv1

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.generic import View
 from django.views.generic import DetailView
 from .models import Like, Post,PostView
 from .forms import CommentForm
+
+
 
 
 class HomeView(View):
@@ -10,11 +13,19 @@ class HomeView(View):
         dashboardinit = request.user.has_perm('core.can_add_post_view')
 
         posts = Post.objects.all()
+        liked_posts = []
+
+        for post in posts:
+            like_query = Like.objects.filter(user=request.user, post=post)
+            liked = like_query.exists()
+            liked_posts.append({'liked': liked})
+
         context = {
-            'posts':posts,
-            'dashboardinit':dashboardinit
+            'posts': posts,  
+            'likes':liked_posts,
+            'dashboardinit': dashboardinit,
         }   
-        return render(request,'post/index.html',context)
+        return render(request, 'post/index.html', context)
     
 
 class DetailPost(DetailView):
@@ -29,6 +40,8 @@ class DetailPost(DetailView):
             return redirect('/')
         return object
     
+    
+    
     def post(self, request, *args, **kwargs):
         form = CommentForm(request.POST)
         
@@ -38,19 +51,31 @@ class DetailPost(DetailView):
             comment.user = request.user
             comment.post = post
             comment.save()
-            return redirect('/')
+            return HttpResponseRedirect(reverse('Detail', args=[post.slug]))  # Asegúrate de incluir el slug en la redirección
+
         
     def get_context_data(self, **kwargs):
+        posts = Post.objects.all()
+        liked_posts = []
+        for post in posts:
+            like_query = Like.objects.filter(user=self.request.user, post=post)
+            liked = like_query.exists()
+            liked_posts.append({'liked': liked})
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
+        context['likes'] = liked_posts
         return context
     
 
 def LikeView(request, slug):
     post = get_object_or_404(Post, slug=slug)
     like_query = Like.objects.filter(user=request.user, post=post)
+
     if like_query.exists():
         like_query[0].delete()
-        return redirect('/')
-    Like.objects.create(user=request.user, post=post)
-    return redirect('/')
+        liked = False
+    else:
+        Like.objects.create(user=request.user, post=post)
+        liked = True
+
+    return HttpResponseRedirect(reverse('Home'))
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from core.models import Post,Categoria,User,Comment
+from core.models import Post,Categoria,User,Comment,Like
 
 admin.site.register(Post)
 admin.site.register(Categoria)
 admin.site.register(User)
+admin.site.register(Like)
 admin.site.register(Comment)
